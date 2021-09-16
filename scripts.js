a5113ba77e75905dba10f68a5bc3d62e7e8a8ede
puneet-twr <90629958+puneet-twr@users.noreply.github.com>
2021-09-16 12:53:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,13 +3,19 @@ let prevList = taskList.innerHTML;
 const input = document.querySelector(".inputTask input");
 // const taskList = document.querySelector(".tasks");
 let deleteButtons = [];
+let order = "Normal";
 
 console.log(input.nodeValue);
 console.log(taskList.innerHTML);
 
 function addTask(task){
     const newTask = "<li><span class=\"function\"><span class=\"check\" onclick='taskFinished(this.parentNode.parentNode)'>&#9745</span><span class=\"delete\" onclick='removeTask(this.parentNode.parentNode)'>&#128503</span></span><span class=\"taskName\">"+task+"</span></li>";
-    taskList.innerHTML= newTask + prevList; 
+    if(order==='Normal'){
+        taskList.innerHTML= newTask + prevList; 
+    }
+    if(order==='Reverse'){
+        taskList.innerHTML= prevList + newTask; 
+    }
     prevList = taskList.innerHTML;
     deleteButtons  = document.querySelectorAll(".delete");
 }
@@ -47,3 +53,34 @@ function taskFinished(item){
     item.childNodes[1].classList.add('checkedTask');
     prevList=taskList.innerHTML;
 }
+
+function deleteChilds(parent){
+    parent.textContent='';
+}
+function reverseSort(){
+    const list = document.getElementById('myList');
+    const liTags = document.querySelectorAll('ul li');
+    const listArr = Array.from(liTags);
+    listArr.reverse();
+    // console.log("number of elements in ul is "+length + " and the no of iteration will be " + iterations);
+    deleteChilds(list);
+    for(let i=0; i< listArr.length; i++){
+        list.appendChild(listArr[i]);
+    }
+    console.log(listArr);
+}
+const rbtns = document.querySelectorAll('input[name=sort]');
+function sortingTasks(e){
+    reverseSort();
+    console.log(e.target.id);
+    if(e.target.id==='n'){
+        order='Normal';
+    }
+    else{
+        order='Reverse';
+    }
+    prevList=taskList.innerHTML;
+}
+rbtns.forEach(rbtn => rbtn.addEventListener('click', function(e){
+    sortingTasks(e);
+}));
